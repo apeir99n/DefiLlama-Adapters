a55c369356c2a93cf0f4f6ab855fd1483ef8cab5
apeir99n <ilgiz.gimaltdinov@gmail.com>
2024-12-10 18:38:22 +0400

    added Gearbox Treasury Fee addresses

--- projects/treasury/gearbox.js
+++ projects/treasury/gearbox.js
@@ -3,6 +3,8 @@ const { nullAddress, treasuryExports } = require("../helper/treasury");
 
 // Treasury
 const treasury = "0x7b065Fcb0760dF0CEA8CFd144e08554F3CeA73D1";
+const treasuryFee = "0x3E965117A51186e41c2BB58b729A1e518A715e5F";
+const treasuryArb = "0x2c31eFFE426765E68A43163A96DD13DF70B53C14";
 const GEAR = "0xBa3335588D9403515223F109EdC4eB7269a9Ab5D";
 
 
@@ -17,7 +19,7 @@ module.exports = treasuryExports({
       ADDRESSES.ethereum.DAI,
       ADDRESSES.ethereum.WSTETH,//wsteth
     ],
-    owners: [treasury],
+    owners: [treasury,treasuryFee,treasuryArb],
     ownTokens: [GEAR]
   },
 })
